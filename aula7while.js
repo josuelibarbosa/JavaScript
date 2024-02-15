94ef1acbdcdf6d81e146be7720c2a1ecefb46ac6
josuelibarbosa <josueli.barbosa@hotmail.com>
2024-02-15 20:43:21 -0300

    ready

---
+++ aula7while.js
@@ -0,0 +1,13 @@
+// While só roda se a condição for verdadeira
+var c = 1;
+while (c <= 10){
+    console.log(c);
+    c++
+}
+
+//Do While roda pelo menos uma vez, independente de ser verdadeiro ou não
+var t = 1;
+do {
+    console.log(t);
+    t++
+}while( t < 1)
--- node_df10ad0192043.tmp.js
+++ node_df10ad0192043.tmp.js
@@ -8,19 +8,26 @@ for (let valor of a){
 
 //2
 const carros = [
-    {id: 1, modelo: "Corsa", marca: "“Chevrolet", preco: 45000, dataCriacao: "01/01/2022"},
-    {id: 2, modelo: "Punto", marca: "Fiat", preco: 35000, dataCriacao: "01/02/2022"},
-    {id: 3, modelo: "Gol", marca: "Volkswagen", preco: 40000, dataCriacao: "01/03/2022"}
+
+    { id: 1, modelo: "Corsa", marca: "“Chevrolet", preco: 45000, dataCriacao: "01/01/2022" },
+  
+    { id: 2, modelo: "Punto", marca: "Fiat", preco: 35000, dataCriacao: "01/02/2022" },
+  
+    { id: 3, modelo: "Gol", marca: "Volkswagen", preco: 40000, dataCriacao: "01/03/2022" }
+  
   ];
   
   let totalPrecos = 0;
   
-  for (let i = 0; i < carros.length; i){
+  for (let i = 0; i < carros.length; i) {
+  
     const carro = carros[i];
+  
     totalPrecos += carro.preco;
+  
   }
+  
   console.log("Total dos preços dos carros:", totalPrecos);
-
 
 // For Each
 a.forEach(valor => console.log(valor))
